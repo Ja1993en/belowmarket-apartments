136b9af438699b17bd65751b7c6bab50f91fc745
Basquiat <jlnmcneal@gmail.com>
2026-06-25 14:41:29 -0500

    Simplify compare added helper text

--- src/pages/PublicPropertyListing.jsx
+++ src/pages/PublicPropertyListing.jsx
@@ -4829,7 +4829,7 @@ function FloorPlanCard({
                 </button>
 
                 {isCompared && (
-                    <p className="mt-3 rounded-lg bg-white px-3 py-2 text-xs font-bold leading-5 text-[#526260] ring-1 ring-[#d7e6df]">
+                    <p className="mt-1.5 text-xs font-bold leading-4 text-[#526260]">
                         Added. Tap View comparison below.
                     </p>
                 )}
