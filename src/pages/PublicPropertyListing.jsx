28530d8da67723dc80d383ac4dfca3ae9e293011
Basquiat <jlnmcneal@gmail.com>
2026-06-25 14:36:09 -0500

    Shorten compare helper message

--- src/pages/PublicPropertyListing.jsx
+++ src/pages/PublicPropertyListing.jsx
@@ -4830,7 +4830,7 @@ function FloorPlanCard({
 
                 {isCompared && (
                     <p className="mt-3 rounded-lg bg-white px-3 py-2 text-xs font-bold leading-5 text-[#526260] ring-1 ring-[#d7e6df]">
-                        Added to compare. Tap View comparison below or scroll up to review your comparison chart.
+                        Added. Tap View comparison below.
                     </p>
                 )}
             </div>
